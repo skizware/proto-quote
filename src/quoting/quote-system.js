e0a595fc581563ae60a43f3cd9c5b718f25cb9be
Dave Anderson <djohnanderson@gmail.com>
2018-03-14 10:13:12 +0200

    Add toJson functionality

--- src/quoting/quote-system.js
+++ src/quoting/quote-system.js
@@ -2,11 +2,16 @@ const KEY_DATE_CREATED = 'dateCreated';
 const KEY_CURRENCY = 'currency';
 const KEY_QUOTE_CFG = 'quoteConfig';
 const KEY_CATEGORY_NAME = 'categoryName';
+const KEY_QUOTE_CATEGORIES = "quoteCategories";
+const KEY_CATEGORY_QUOTE_ITEMS_LIST = "quoteItems";
+const KEY_QUOTE_ITEM_MARKUP_PERCENT = "markupPercent";
 const KEY_QUOTE_ITEM_NAME = 'itemName';
 const KEY_QUOTE_ITEM_RATE = 'itemRate';
 const KEY_QUOTE_ITEM_OVERALL_QUANTITY_LABEL = 'totalQuantityUnit';
 const KEY_QUOTE_ITEM_SUB_ITEMS = "subQuoteItems";
 const KEY_QUOTE_ITEM_QUANTITY_AND_UNITS_LIST = "itemQuantityAndUnitsList";
+const KEY_QUANTITIES_LIST = 'quantities';
+const KEY_SUB_CATEGORIES_LIST = "subCategories";
 const KEY_COMPOSITE_QUANTITY_UNIT = "unit";
 const KEY_LABELED_QUANTITY = "quantity";
 const KEY_LABELED_QUANTITY_LABEL = "label";
@@ -15,15 +20,28 @@ const KEY_LABELED_QUANTITY_LABEL = "label";
 class Quote {
 
     constructor(data) {
-        console.log(data);
         this._quoteCategories = [];
-        this._dateCreated = data[KEY_DATE_CREATED];
+        this._dateCreated = data[KEY_DATE_CREATED] || Date.parse(new Date().toUTCString());
         this._currency = data[KEY_CURRENCY];
         this._quoteConfig = data[KEY_QUOTE_CFG];
 
-        data["quoteCategories"].map((categoryData) => {
-            this._quoteCategories.push(new ParentCategory(categoryData));
+        if (data[KEY_QUOTE_CATEGORIES]) {
+            data[KEY_QUOTE_CATEGORIES].map((categoryData) => {
+                this._quoteCategories.push(new ParentCategory(categoryData));
+            });
+        }
+    }
+
+    toJson() {
+        let jsonRepresentation = {};
+        jsonRepresentation[KEY_DATE_CREATED] = this._dateCreated;
+        jsonRepresentation[KEY_CURRENCY] = this._currency;
+        jsonRepresentation[KEY_QUOTE_CFG] = this._quoteConfig;
+        jsonRepresentation[KEY_QUOTE_CATEGORIES] = [];
+        this._quoteCategories.map((quoteCategory) => {
+            jsonRepresentation[KEY_QUOTE_CATEGORIES].push(quoteCategory.toJson());
         });
+        return jsonRepresentation;
     }
 
 }
@@ -33,9 +51,21 @@ class QuoteCategory {
     constructor(data) {
         this._categoryName = data[KEY_CATEGORY_NAME];
         this._quoteItems = [];
-        data["quoteItems"].map((quoteItemData) => {
-            this._quoteItems.push(new ParentQuoteItem(quoteItemData));
+        if (data[KEY_CATEGORY_QUOTE_ITEMS_LIST]) {
+            data[KEY_CATEGORY_QUOTE_ITEMS_LIST].map((quoteItemData) => {
+                this._quoteItems.push(new ParentQuoteItem(quoteItemData));
+            });
+        }
+    }
+
+    toJson() {
+        let jsonRepresentation = {};
+        jsonRepresentation[KEY_CATEGORY_NAME] = this._categoryName;
+        jsonRepresentation[KEY_CATEGORY_QUOTE_ITEMS_LIST] = [];
+        this._quoteItems.map((quoteItem) => {
+            jsonRepresentation[KEY_CATEGORY_QUOTE_ITEMS_LIST].push(quoteItem.toJson());
         });
+        return jsonRepresentation;
     }
 }
 
@@ -44,9 +74,20 @@ class ParentCategory extends QuoteCategory {
     constructor(data) {
         super(data);
         this._subCategories = [];
-        data["subCategories"].map((subCategoryData) => {
-            this._subCategories.push(new QuoteCategory(subCategoryData));
+        if (data[KEY_SUB_CATEGORIES_LIST]) {
+            data[KEY_SUB_CATEGORIES_LIST].map((subCategoryData) => {
+                this._subCategories.push(new QuoteCategory(subCategoryData));
+            });
+        }
+    }
+
+    toJson() {
+        let jsonRepresentation = super.toJson();
+        jsonRepresentation[KEY_SUB_CATEGORIES_LIST] = [];
+        this._subCategories.map((subCategory) => {
+            jsonRepresentation[KEY_SUB_CATEGORIES_LIST].push(subCategory.toJson());
         });
+        return jsonRepresentation;
     }
 
 }
@@ -56,12 +97,27 @@ class QuoteItem {
     constructor(data) {
         this._itemName = data[KEY_QUOTE_ITEM_NAME];
         this._itemRate = data[KEY_QUOTE_ITEM_RATE];
-        this._itemMarkup = undefined;
+        this._itemMarkup = data[KEY_QUOTE_ITEM_MARKUP_PERCENT];
         this._totalQuantityLabel = data[KEY_QUOTE_ITEM_OVERALL_QUANTITY_LABEL];
         this._compositeQuantityList = [];
-        data[KEY_QUOTE_ITEM_QUANTITY_AND_UNITS_LIST].map((compositeQuantityData) => {
-            this._compositeQuantityList.push(new CompositeQuantity(compositeQuantityData));
+        if (data[KEY_QUOTE_ITEM_QUANTITY_AND_UNITS_LIST]) {
+            data[KEY_QUOTE_ITEM_QUANTITY_AND_UNITS_LIST].map((compositeQuantityData) => {
+                this._compositeQuantityList.push(new CompositeQuantity(compositeQuantityData));
+            });
+        }
+    }
+
+    toJson() {
+        let jsonRepresentation = {};
+        jsonRepresentation[KEY_QUOTE_ITEM_NAME] = this._itemName;
+        jsonRepresentation[KEY_QUOTE_ITEM_RATE] = this._itemRate;
+        jsonRepresentation[KEY_QUOTE_ITEM_MARKUP_PERCENT] = this._itemMarkup;
+        jsonRepresentation[KEY_QUOTE_ITEM_OVERALL_QUANTITY_LABEL] = this._totalQuantityLabel;
+        jsonRepresentation[KEY_QUOTE_ITEM_QUANTITY_AND_UNITS_LIST] = [];
+        this._compositeQuantityList.map((compositeQuantity) => {
+            jsonRepresentation[KEY_QUOTE_ITEM_QUANTITY_AND_UNITS_LIST].push(compositeQuantity.toJson());
         });
+        return jsonRepresentation;
     }
 }
 
@@ -70,9 +126,20 @@ class ParentQuoteItem extends QuoteItem {
     constructor(data) {
         super(data);
         this._childQuoteItems = [];
-        data[KEY_QUOTE_ITEM_SUB_ITEMS].map((subQuoteItemData) => {
-            this._childQuoteItems.push(new QuoteItem(subQuoteItemData));
+        if (data[KEY_QUOTE_ITEM_SUB_ITEMS]) {
+            data[KEY_QUOTE_ITEM_SUB_ITEMS].map((subQuoteItemData) => {
+                this._childQuoteItems.push(new QuoteItem(subQuoteItemData));
+            });
+        }
+    }
+
+    toJson() {
+        let jsonRepresentation = super.toJson();
+        jsonRepresentation[KEY_QUOTE_ITEM_SUB_ITEMS] = [];
+        this._childQuoteItems.map((childQuoteItem) => {
+            jsonRepresentation[KEY_QUOTE_ITEM_SUB_ITEMS].push(childQuoteItem.toJson())
         });
+        return jsonRepresentation;
     }
 }
 
@@ -82,29 +149,41 @@ class LabeledQuantity {
         this._quantity = quantity;
         this.label = label;
     }
+
+    toJson() {
+        let jsonRepresentation = {};
+        jsonRepresentation[KEY_LABELED_QUANTITY] = this._quantity;
+        jsonRepresentation[KEY_LABELED_QUANTITY_LABEL] = this.label;
+        return jsonRepresentation;
+    }
 }
 
 class CompositeQuantity {
 
-    /*constructor(labeledQuantityList, overallUnit) {
-        this._labeledQuantityList = labeledQuantityList;
-        this._overallUnit = overallUnit;
-    }*/
-
     constructor(data) {
         this._labeledQuantityList = [];
         this._overallUnit = data[KEY_COMPOSITE_QUANTITY_UNIT];
-        if (data.quantities) {
-            data.quantities.map((labeledQuantityData) => {
+        if (data[KEY_QUANTITIES_LIST]) {
+            data[KEY_QUANTITIES_LIST].map((labeledQuantityData) => {
                 this._labeledQuantityList.push(new LabeledQuantity(labeledQuantityData[KEY_LABELED_QUANTITY], labeledQuantityData[KEY_LABELED_QUANTITY_LABEL]));
             });
         }
+    }
+
+    toJson() {
+        let jsonRepresentation = {};
+        jsonRepresentation[KEY_COMPOSITE_QUANTITY_UNIT] = this._overallUnit;
+        jsonRepresentation[KEY_QUANTITIES_LIST] = [];
+        this._labeledQuantityList.map((labeledQuantity) => {
+            jsonRepresentation[KEY_QUANTITIES_LIST].push(labeledQuantity.toJson());
+        });
+        return jsonRepresentation;
     }
 }
 
 
 const testData = {
-    dateCreated: "2018-02-22",
+    dateCreated: 1521014398000,
     currency: "ZAR",
     quoteConfig: {
         markUpRate: 1.12
